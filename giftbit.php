724e85664816add435c32e017fbf04456bab1800
Andrii Severynenko <andriy.severynenko@ffwagency.com>
2018-08-09 19:25:43 +0300

    Implement main API calls.

--- giftbit.php
+++ giftbit.php
@@ -11,17 +11,273 @@
     private $client;
 
     /**
+     * Giftbit constructor.
+     *
+     * @param string $endpoint
+     *   Giftbit endpoint URL.
+     * @param string $auth_token
+     *   Giftbit Auth Token.
+     *
      * @throws \Exception
      */
-    public function __construct($auth_token) {
+    public function __construct($endpoint, $auth_token) {
+      if (!$endpoint)
+        throw new Exception("Giftbit Client endpoint parameter is required");
       if (!$auth_token)
         throw new Exception("Giftbit Client auth_token parameter is required");
 
-      $this->client = new Giftbit\Client($auth_token);
+      $this->client = new Giftbit\Client($endpoint, $auth_token);
     }
 
+    /**
+     * Ping call.
+     *
+     * @param int $http_code
+     *   HTTP Response code.
+     *
+     * @return string
+     *   JSON response.
+     */
     public function ping(&$http_code = NULL) {
       return $this->client->ping($http_code);
+    }
+
+    /**
+     * Brands call.
+     *
+     * @param array $request_data
+     *   Request data array. Could contain:
+     *   - region,
+     *   - max_price_in_cents,
+     *   - min_price_in_cents,
+     *   - currencyisocode (CAD, USD, AUD),
+     *   - search,
+     *   - limit (default: 20),
+     *   - offset (default: 0).
+     * @param int $http_code
+     *   HTTP Response code.
+     *
+     * @return string
+     *   JSON response.
+     */
+    public function brands(array $request_data, &$http_code = NULL) {
+      return $this->client->brands($request_data, $http_code);
+    }
+
+    /**
+     * Get brand.
+     *
+     * @param string $brand_code
+     *   Giftbit brand code.
+     * @param int $http_code
+     *   HTTP Response code.
+     *
+     * @return string
+     *   JSON response.
+     *
+     * @throws \Exception
+     */
+    public function brand($brand_code, &$http_code = NULL) {
+      if (!$brand_code)
+        throw new Exception("Giftbit Client brand_code parameter is required");
+
+      return $this->client->brand($brand_code, $http_code);
+    }
+
+    /**
+     * Regions call.
+     *
+     * @param int $http_code
+     *   HTTP Response code.
+     *
+     * @return string
+     *   JSON response.
+     */
+    public function regions(&$http_code = NULL) {
+      return $this->client->regions($http_code);
+    }
+
+    /**
+     * Create campaign.
+     *
+     * @param array $request_data
+     *   Request data array. Could contain:
+     *   - message (required if no supplied gift_template),
+     *   - subject (required if no supplied gift_template),
+     *   - gift_template (template ID from the Giftbit dashboard),
+     *   - delivery_type (SHORTLINK, GIFTBIT_EMAIL),
+     *   - link_count (in case of SHORTLINK only),
+     *   - contacts (array of contacts with fields:
+     *       - email,
+     *       - firstname,
+     *       - lastname,
+     *     ),
+     *   - price_in_cents (2500 = $25),
+     *   - brand_codes (array of brand codes gotten from /brands API call),
+     *   - expiry (YYYY-MM-DD),
+     *   - id (just unique string).
+     * @param int $http_code
+     *   HTTP Response code.
+     *
+     * @return string
+     *   JSON response.
+     */
+    public function createCampaign(array $request_data, &$http_code = NULL) {
+      return $this->client->createCampaign($request_data, $http_code);
+    }
+
+    /**
+     * Get campaign.
+     *
+     * @param string $campaign_id
+     *   Giftbit campaign ID.
+     * @param int $http_code
+     *   HTTP Response code.
+     *
+     * @return string
+     *   JSON response.
+     *
+     * @throws \Exception
+     */
+    public function getCampaign($campaign_id, &$http_code = NULL) {
+      if (!$campaign_id)
+        throw new Exception("Giftbit Client campaign_id parameter is required");
+
+      return $this->client->getCampaign($campaign_id, $http_code);
+    }
+
+    /**
+     * Gifts call.
+     *
+     * @param array $request_data
+     *   Request data array. Could contain:
+     *   - uuid,
+     *   - campaign_uuid,
+     *   - campaign_id,
+     *   - price_in_cents_greater_than,
+     *   - price_in_cents_less_than,
+     *   - recipient_name,
+     *   - recipient_email,
+     *   - delivery_status (UNSENT, DELIVERED, UNDELIVERABLE,
+     *     TEMPORARILY_UNDELIVERABLE, UNSUBSCRIBED, COMPLAINT),
+     *   - status (SENT_AND_REDEEMABLE, REDEEMED, TO_CHARITY, GIVER_CANCELLED,
+     *     or EXPIRED),
+     *   - created_date_greater_than (yyyy-MM-dd HH:mm:ss),
+     *   - created_date_less_than (yyyy-MM-dd HH:mm:ss),
+     *   - delivery_date_greater_than (yyyy-MM-dd HH:mm:ss),
+     *   - delivery_date_less_than (yyyy-MM-dd HH:mm:ss),
+     *   - redelivery_count_greater_than,
+     *   - redelivery_count_less_than,
+     *   - redeemed_date_greater_than (yyyy-MM-dd HH:mm:ss),
+     *   - redeemed_date_less_than (yyyy-MM-dd HH:mm:ss),
+     *   - limit (default: 20),
+     *   - offset (default: 0),
+     *   - sort (campaign_id, price_in_cents, recipient_name, recipient_email,
+     *     delivery_status, or status),
+     *   - order (asc or desc).
+     * @param int $http_code
+     *   HTTP Response code.
+     *
+     * @return string
+     *   JSON response.
+     */
+    public function gifts(array $request_data, &$http_code = NULL) {
+      return $this->client->gifts($request_data, $http_code);
+    }
+
+    /**
+     * Get gift.
+     *
+     * @param string $gift_uuid
+     *   Giftbit gift UUID.
+     * @param int $http_code
+     *   HTTP Response code.
+     *
+     * @return string
+     *   JSON response.
+     *
+     * @throws \Exception
+     */
+    public function getGift($gift_uuid, &$http_code = NULL) {
+      if (!$gift_uuid)
+        throw new Exception("Giftbit Client gift_uuid parameter is required");
+
+      return $this->client->getGift($gift_uuid, $http_code);
+    }
+
+    /**
+     * Resend gift.
+     *
+     * @param string $gift_uuid
+     *   Giftbit gift UUID.
+     * @param int $http_code
+     *   HTTP Response code.
+     *
+     * @return string
+     *   JSON response.
+     *
+     * @throws \Exception
+     */
+    public function resendGift($gift_uuid, &$http_code = NULL) {
+      if (!$gift_uuid)
+        throw new Exception("Giftbit Client gift_uuid parameter is required");
+
+      return $this->client->resendGift($gift_uuid, $http_code);
+    }
+
+    /**
+     * Cancel gift.
+     *
+     * @param string $gift_uuid
+     *   Giftbit gift UUID.
+     * @param int $http_code
+     *   HTTP Response code.
+     *
+     * @return string
+     *   JSON response.
+     *
+     * @throws \Exception
+     */
+    public function cancelGift($gift_uuid, &$http_code = NULL) {
+      if (!$gift_uuid)
+        throw new Exception("Giftbit Client gift_uuid parameter is required");
+
+      return $this->client->cancelGift($gift_uuid, $http_code);
+    }
+
+    /**
+     * Get campaign.
+     *
+     * @param string $campaign_id
+     *   Giftbit campaign ID.
+     * @param array $request_data
+     *   Request data array. Could contain: limit, offset.
+     * @param int $http_code
+     *   HTTP Response code.
+     *
+     * @return string
+     *   JSON response.
+     *
+     * @throws \Exception
+     */
+    public function links($campaign_id, array $request_data, &$http_code = NULL) {
+      if (!$campaign_id)
+        throw new Exception("Giftbit Client campaign_id parameter is required");
+
+      return $this->client->links($campaign_id, $request_data, $http_code);
+    }
+
+    /**
+     * Funds call.
+     *
+     * @param int $http_code
+     *   HTTP Response code.
+     *
+     * @return string
+     *   JSON response.
+     */
+    public function funds(&$http_code = NULL) {
+      return $this->client->funds($http_code);
     }
 
   }
--- giftbit/client.php
+++ giftbit/client.php
@@ -4,51 +4,139 @@
 
   class Client {
 
-    private $auth_token;
+    private $endpoint;
+    private $authToken;
 
-    public function __construct($auth_token) {
-      $this->auth_token = $auth_token;
+    public function __construct($endpoint, $auth_token) {
+      $this->endpoint = $endpoint;
+      $this->authToken = $auth_token;
     }
 
     public function ping(&$http_code = NULL) {
-      $endpoint = "https://api-testbed.giftbit.com/papi/v1/ping";
-      $request_data = array(
-        'auth_token' => $this->auth_token,
-      );
+      $endpoint = $this->endpoint . '/ping';
 
-      return $this->_send($endpoint, $request_data, 'get', $http_code);
+      return $this->_send($endpoint, array(), $http_code);
     }
 
-    private function _send($endpoint, $request_data, $request_type = 'post', &$http_code = NULL) {
-      $auth_token = '';
+    public function brands(array $request_data, &$http_code = NULL) {
+      $endpoint = $this->endpoint . '/brands';
+
+      return $this->_send($endpoint, $request_data, $http_code);
+    }
+
+    public function brand($brand_code, &$http_code = NULL) {
+      $endpoint = $this->endpoint . '/brands/' . $brand_code;
+
+      return $this->_send($endpoint, array(), $http_code);
+    }
+
+    public function regions(&$http_code = NULL) {
+      $endpoint = $this->endpoint . '/regions';
+
+      return $this->_send($endpoint, array(), $http_code);
+    }
+
+    public function createCampaign(array $request_data, &$http_code = NULL) {
+      $endpoint = $this->endpoint . '/campaign';
+
+      return $this->_send($endpoint, $request_data, $http_code, 'post');
+    }
+
+    public function getCampaign($campaign_id, &$http_code = NULL) {
+      $endpoint = $this->endpoint . '/campaign/' . $campaign_id;
+
+      return $this->_send($endpoint, array(), $http_code);
+    }
+
+    public function gifts(array $request_data, &$http_code = NULL) {
+      $endpoint = $this->endpoint . '/gifts';
+
+      return $this->_send($endpoint, $request_data, $http_code);
+    }
+
+    public function getGift($gift_uuid, &$http_code = NULL) {
+      $endpoint = $this->endpoint . '/gifts/' . $gift_uuid;
+
+      return $this->_send($endpoint, array(), $http_code);
+    }
+
+    public function resendGift($gift_uuid, &$http_code = NULL) {
+      $endpoint = $this->endpoint . '/gifts/' . $gift_uuid;
+      $request_data = array(
+        'resend' => TRUE,
+      );
+
+      return $this->_send($endpoint, $request_data, $http_code, 'put');
+    }
+
+    public function cancelGift($gift_uuid, &$http_code = NULL) {
+      $endpoint = $this->endpoint . '/gifts/' . $gift_uuid;
+
+      return $this->_send($endpoint, array(), $http_code, 'delete');
+    }
+
+    public function links($campaign_id, array $request_data, &$http_code = NULL) {
+      $endpoint = $this->endpoint . '/links/' . $campaign_id;
+
+      return $this->_send($endpoint, $request_data, $http_code);
+    }
+
+    public function funds(&$http_code = NULL) {
+      $endpoint = $this->endpoint . '/funds';
+
+      return $this->_send($endpoint, array(), $http_code);
+    }
+
+    private function _send($endpoint, array $request_data, &$http_code = NULL, $request_type = 'get') {
       if (isset($request_data['auth_token'])) {
-        $auth_token = $request_data['auth_token'];
         unset($request_data['auth_token']);
       }
-      $request_data = json_encode($request_data);
 
       $headers = array(
         'Accept: application/json',
         'Content-Type: application/json',
-        'Authorization: Bearer ' . $auth_token,
+        'Authorization: Bearer ' . $this->authToken,
       );
 
       $handle = curl_init();
-      curl_setopt($handle, CURLOPT_URL, $endpoint);
+      curl_setopt($handle, CURLOPT_HEADER, FALSE);
       curl_setopt($handle, CURLOPT_HTTPHEADER, $headers);
       curl_setopt($handle, CURLOPT_RETURNTRANSFER, TRUE);
       curl_setopt($handle, CURLOPT_SSL_VERIFYHOST, FALSE);
       curl_setopt($handle, CURLOPT_SSL_VERIFYPEER, FALSE);
       switch ($request_type) {
+        case 'get':
+          $query = http_build_query($request_data);
+          curl_setopt($handle, CURLOPT_URL, $endpoint . '?' . $query);
+          break;
+
         case 'post':
+          curl_setopt($handle, CURLOPT_URL, $endpoint);
           curl_setopt($handle, CURLOPT_POST, TRUE);
+          if (!empty($request_data)) {
+            $request_data = json_encode($request_data);
+            curl_setopt($handle, CURLOPT_POSTFIELDS, $request_data);
+          }
           break;
 
         case 'put':
-          curl_setopt($handle, CURLOPT_CUSTOMREQUEST, "PUT");
+          curl_setopt($handle, CURLOPT_URL, $endpoint);
+          curl_setopt($handle, CURLOPT_CUSTOMREQUEST, 'PUT');
+          if (!empty($request_data)) {
+            $request_data = json_encode($request_data);
+            curl_setopt($handle, CURLOPT_POSTFIELDS, $request_data);
+          }
+          break;
+
+        case 'delete':
+          curl_setopt($handle, CURLOPT_URL, $endpoint);
+          curl_setopt($handle, CURLOPT_CUSTOMREQUEST, 'DELETE');
+          if (!empty($request_data)) {
+            $request_data = json_encode($request_data);
+            curl_setopt($handle, CURLOPT_POSTFIELDS, $request_data);
+          }
           break;
       }
-      curl_setopt($handle, CURLOPT_POSTFIELDS, $request_data);
 
       $result = curl_exec($handle);
       $http_code = curl_getinfo($handle, CURLINFO_HTTP_CODE);
